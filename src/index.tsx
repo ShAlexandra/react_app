c422d989d8d7438a581725c774fece77eba41ae4
Alex Shulga <shalexandra2000@gmail.com>
2022-12-14 00:35:52 +0300

    store + slices

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './styles/index.scss'
@@ -9,9 +11,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
