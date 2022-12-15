3da329c301ff5a038937d4788c8dd14cd5177ac9
Alex Shulga <shalexandra2000@gmail.com>
2022-12-15 03:27:19 +0300

    api ok

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,17 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import currentWeatherSliceReduser from './slices/currentWeatherSlise'
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+    currentWeatherSliceReduser
+})
 
 export const store = configureStore( {
     reducer: rootReducer,
+    middleware: ((getDefaultMiddleware) => getDefaultMiddleware(
+        {
+            serializableCheck: false
+        }
+    ))
 })
 
 export type RootState = ReturnType<typeof rootReducer>
