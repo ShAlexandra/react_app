af4615d4cb6911a7202bf256aed5c9647433a876
Alex Shulga <shalexandra2000@gmail.com>
2022-12-16 19:25:02 +0300

    api is ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Home } from './pages/Home/components/Home';
+import { Home } from './pages/Home/Home';
 import { Header } from './shared/Header/Header';
 
 function App() {
--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -1,10 +1,9 @@
-import axios, { Axios, AxiosResponse } from "axios";
-import api from "../axios";
-import { Weather } from "../store/types/types";
+import { AxiosResponse } from 'axios';
+import api from '../axios';
+import { Weather } from '../store/types/types';
 
 export class WeatherService {
-    static getCurrentWeather(city: string):
-    Promise<AxiosResponse<Weather>> {
-        return api.get<Weather>('/weather?q=${city}')
-    }
+  static getCurrentWeather(city: string): Promise<AxiosResponse<Weather>> {
+    return api.get<Weather>(`/weather?q=${city}`);
+  }
 }
