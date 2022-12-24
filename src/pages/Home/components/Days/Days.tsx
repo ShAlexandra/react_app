9b04903127f0ff29e0abeac2dffe311a6e41fc69
Alex Shulga <shalexandra2000@gmail.com>
2022-12-24 17:39:43 +0300

    trying...

--- src/pages/Home/components/Days/Days.tsx
+++ src/pages/Home/components/Days/Days.tsx
@@ -1,75 +1,70 @@
 import React from 'react'
+import { Weather, WeatherList } from '../../../../store/types/types';
 import { Card } from './Card';
 
 import _s from './Days.module.scss'
 
-interface Props {}
+interface Props {
+    weatherList: WeatherList
+}
 
 export interface Day {
     day: string;
     day_info: string;
     icon_id: string;
     temp_day: string;
-    temp_night: string;
     info: string;
 }
 
-export const Days = (props: Props) => {
+export const Days = ({ weatherList }: Props) => {
     const days: Day[] = [
         {
             day: 'Сегодня',
             day_info: '28 авг',
             icon_id: 'today-img',
-            temp_day: '+18',
-            temp_night: '+15',
+            temp_day: '${weatherList.main.weatherList[0].main.temp}',
             info: 'облачно'
         },
         {
             day: 'Завтра',
             day_info: '29 авг',
             icon_id: 'today-img',
-            temp_day: '+18',
-            temp_night: '+15',
+            temp_day: '${weatherList.main.weatherList[1].main.temp}',
             info: 'облачно'
         },
         {
             day: 'Ср',
             day_info: '30 авг',
             icon_id: 'today-img',
-            temp_day: '+18',
-            temp_night: '+15',
+            temp_day: '${weatherList.main.weatherList[0].main.temp}',
             info: 'облачно'
         },
         {
             day: 'Чт',
             day_info: '31 авг',
             icon_id: 'today-img',
-            temp_day: '+18',
-            temp_night: '+15',
+            temp_day: '${weatherList.main.weatherList[0].main.temp}',
             info: 'облачно'
         },
         {
             day: 'Пт',
             day_info: '1 сен',
             icon_id: 'today-img',
-            temp_day: '+18',
-            temp_night: '+15',
+            temp_day: '${weatherList.main.weatherList[0].main.temp}',
             info: 'облачно'
         },
         {
             day: 'Сб',
             day_info: '2 сен',
             icon_id: 'today-img',
-            temp_day: '+18',
-            temp_night: '+15',
+            temp_day: '${weatherList.main.weatherList[0].main.temp}',
             info: 'облачно'
         },
         {
             day: 'Вс',
             day_info: '3 сен',
             icon_id: 'today-img',
-            temp_day: '+18',
-            temp_night: '+15',
+            temp_day: '${weatherList.main.weatherList[0].main.temp}',
             info: 'облачно'
         }
     ];
